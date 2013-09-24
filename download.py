0535911b5505262da5ada78cc4904b0c002ab8f5
Thomas Levine <occurrence@thomaslevine.com>
2013-09-24 14:50:41 +0000

    download yay

--- download.py
+++ download.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import os
 
 from download_ckan import download as ckan
 import portals
 
-ckan("http://demo.ckan.org", '/tmp/ckan')
+ROOT_DIR = 'portals'
+
+for portal in portals.ckan:
+    ckan("http://" + portal, os.path.join(ROOT_DIR, 'ckan', portal))
