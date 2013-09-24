01f8acb6576a3d14a70b02c68d5ec90770b2bb8f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-24 14:56:33 +0000

    threads

--- download.py
+++ download.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 import os
+import threading
 
 from download_ckan import download as ckan
 import portals
 
 ROOT_DIR = 'portals'
 
+threads = {}
+
 for portal in portals.ckan:
-    ckan("http://" + portal, os.path.join(ROOT_DIR, 'ckan', portal))
+    args = ("http://" + portal, os.path.join(ROOT_DIR, 'ckan', portal))
+    threads[('ckan', portal)] = threading.Thread(target = ckan, args = args)
+
+
+'''
+    thr.start() # will run "foo"
+    thr.is_alive() # will return whether foo is running currently
+    thr.join()
+'''
