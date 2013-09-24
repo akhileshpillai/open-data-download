dab975581375867e5cf2ab304b8f1b28083ff226
Thomas Levine <occurrence@thomaslevine.com>
2013-09-24 14:38:44 +0000

    downloading ckan

--- download_ckan.py
+++ download_ckan.py
@@ -1,7 +1,23 @@
 # https://twitter.com/CKANproject/status/378182161330753536
 import ckanapi
 import pprint
+from time import sleep
 
-demo = ckanapi.RemoteCKAN('http://demo.ckan.org')
-groups = demo.action.group_list(id='data-explorer')
-pprint.pprint(groups)
+from portals import ckan
+
+def download(portal, directory):
+    '''
+    Args:
+        portal: A string for the root of the portal (like "http://demo.ckan.org")
+        directory: The directory to which stuff should be saved
+    Returns:
+        Nothing
+    '''
+    portal = ckanapi.RemoteCKAN(portal)
+    datasets = portal.action.package_list()
+    for dataset in datasets:
+        _save_dataset(id)
+        sleep(3)
+
+def _save_dataset(id)
+        portal.action.package_show(id=id)
