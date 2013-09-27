7dc35732235fecfd8fa1e6d73b6b5c0f15396b43
Thomas Levine <occurrence@thomaslevine.com>
2013-09-27 12:22:49 -0700

    download socrata

--- download.py
+++ download.py
@@ -3,3 +3,4 @@ from download_ckan import main as ckan
 from download_socrata import main as socrata
 
 # ckan()
+# socrata()
--- download_socrata.py
+++ download_socrata.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python2
 '''
 https://github.com/jasonlally/open-data-browser/blob/dev/data/dataportalapi.py
 '''
+from urllib import urlretrieve
+import portals
+
+def download(portal_url, directory):
+    '''
+    Args:
+        portal: A string for the root of the portal (like "http://demo.ckan.org")
+        directory: The directory to which stuff should be saved
+    Returns:
+        Nothing
+    '''
+    # Make sure the directory exists.
+    try:
+        os.makedirs(directory)
+    except OSError:
+        pass
+
+		url = 'http://' + portal_url + '/api/search/views.json'
+    urlretrieve(url, os.path.join(directory, portal_url))
+
+def main():
+    for portal in portals:
+        download(portal, os.path.join('portals', 'socrata'))
