1d925650b5671f4e4593a718b046f64244481968
Thomas Levine <occurrence@thomaslevine.com>
2013-09-27 14:48:44 -0700

    download socrata from search page

--- download_socrata.py
+++ download_socrata.py
@@ -1,6 +1,7 @@
 from time import sleep
 import os, json
 
+from lxml.html import fromstring
 from urllib2 import urlopen
 from urllib import urlretrieve
 
@@ -20,5 +21,28 @@ def download(portal_url, directory):
     except OSError:
         pass
 
-    portal_url
-    html.xpath('//a[text()="This site only"]/@href')
+    html = fromstring(urlopen(u'https://' + portal_url + u'/browse/embed?limitTo=datasets&q=&utf8=%E2%9C%93&view_type=table').read())
+    search_base = u'https://' + portal_url + html.xpath(u'//a[text()="This site only"]/@href')[0]
+
+    page = 1
+    while True:
+        ids = parse_search_page(search_base, page)
+        if len(ids) == 0:
+            break
+        else:
+            for id in ids:
+                download_view(portal_url, directory, id)
+            page += 1
+
+def download_view(portal_url, directory, id):
+    url = "http://%s/views/%s.json" % (portal_url, id)
+    urlretrieve(url, os.path.join(directory, id))
+
+def parse_search_page(search_base, number):
+    'Get 4x4s out of a search page.'
+    html = fromstring(urlopen(search_base + '&page=%d' % number).read())
+    hrefs = html.xpath('//td[@class="nameDesc"]/a/@href')
+    return [href.split('/')[-1] for href in hrefs]
+
+if __name__ == '__main__':
+    download(u'data.hawaii.gov', u'portals/socrata')
