05756195e6b2e70fd052a74f7b1d1ee53ab6ffbd
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 11:31:34 -0400

    male versus female

--- query-gender.py
+++ query-gender.py
@@ -6,7 +6,11 @@ from standardize import iter_datasets, map_reduce
 
 def has_gender(string):
     wacky_string = ' ' + string.lower().replace('_', ' ') + ' '
-    return re.match(r'^.* (men|man|males?|girls?|boys?) .*$', wacky_string) != None
+    return re.match(r'^.* ((wo)?men|(wo)?man|(fe)?males?|girls?|boys?) .*$', wacky_string) != None
+
+def has_male(string):
+    wacky_string = ' ' + string.lower().replace('_', ' ') + ' '
+    return re.match(r'^.* (men|man|males?|boys?) .*$', wacky_string) != None
 
 def has_female(string):
     wacky_string = ' ' + string.lower().replace('_', ' ') + ' '
@@ -30,25 +34,22 @@ def how_many(pair):
         A dictionary
     '''
     uri, column_names = pair
-    n_gendered = len(filter(has_gender, column_names))
-    n_female   = len(filter(has_female, column_names))
     return {
         'uri': uri,
-        'n_gendered': n_gendered,
-        'n_female': n_female,
+        'n_gendered': len(filter(has_gender, column_names)),
+        'n_female': len(filter(has_female, column_names)),
+        'n_male': len(filter(has_male, column_names)),
+        'columns': column_names,
+        'gendered_columns': filter(has_gender, column_names),
+        'male_columns': filter(has_male, column_names),
+        'female_columns': filter(has_female, column_names),
     }
 
 def go(datasets):
     # Subset
     columns = list(map_reduce(lambda d: (d['uri'], d['columns']), datasets = datasets))
     gendered_datasets = filter(gendered, columns)
-
-    g_dict = dict(zip((d[0] for d in gendered_datasets), zip(map(how_many, gendered_datasets), map(gendered_columns, (d[1] for d in gendered_datasets)), (d[1] for d in gendered_datasets))))
-    for k,v in g_dict.items():
-        g_dict[k] = v[0]
-        g_dict[k]['gendered_columns'] = v[1]
-        g_dict[k]['all_columns'] = v[2]
-
+    g_dict = map(how_many, gendered_datasets)
     return g_dict
     # g_df = pandas.DataFrame(g_dict)[['uri', 'n_gendered', 'n_female']]
 
