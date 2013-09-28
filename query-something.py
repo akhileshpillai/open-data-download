88467fdaf2c7cdb087af0f2b75265fccb9374dce
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 12:27:00 -0400

    oeu

--- query-something.py
+++ query-something.py
@@ -4,7 +4,7 @@ from standardize import iter_datasets, map_reduce
 
 def standard_license(raw_license):
     r = raw_license.lower()
-    if re.match('.*(public domain|cczero).*', r):
+    if re.match('.*(Dom.nio P.blico|domaine public|public domain|cczero).*', r):
         return 'Public domain'
     elif re.match('.*(odbl|open database license).*', r):
         return 'ODbL'
@@ -12,22 +12,41 @@ def standard_license(raw_license):
         return 'Not specified'
     elif re.match(r'^(cc|creative commons)(.*)', r):
         return creative_commons(re.match(r'^(cc|creative commons)(.*)', r).group(2))
+    elif re.match(r'^.*(open data).*', r):
+        return 'Other open data license'
+    elif r == u'GNU Free Documentation License':
+        return 'GFDL'
+    elif re.match(r'^.*(open|oiuverte|libre|ouverte|aberta|abierta).*', r):
+        return 'Other open license'
     else:
-        return raw_license
+        # print raw_license
         return 'Other'
 
 def creative_commons(r):
     attribution = ''
     share_alike = ''
     non_commercial = ''
+    no_derivative = ''
+
     if re.match(r'.*(attribution|by).*', r):
         attribution = '-BY'
-    elif re.match(r'.*(share-alike|sa).*', r):
+
+    if re.match(r'.*(share.?alike|sa).*', r):
         share_alike = '-SA'
-    elif re.match(r'.*(non-commerical|nc).*', r):
+
+    if re.match(r'.*(non.?commerical|nc).*', r):
         non_commercial = '-NC'
 
-    return 'CC' + attribution + share_alike + non_commercial
+    if re.match(r'.*(derivative|nd).*', r):
+        no_derivative = '-ND'
+
+    if 'universal' in r:
+        return 'Public domain'
+    else:
+        a = 'CC' + attribution + share_alike + no_derivative + non_commercial
+        if a == 'CC':
+            print r
+        return a
 
 '''
 # datasets = list(iter_datasets())
