344f5f62e218412556ece05f4011ffd1356c9721
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 11:05:04 -0400

    more querying

--- query-gender.py
+++ query-gender.py
@@ -1,17 +1,51 @@
+import re
+
+import pandas
+
 from standardize import iter_datasets, map_reduce
 
-# load the data into memory
-datasets = list(iter_datasets())
+def has_gender(string):
+    wacky_string = ' ' + string.lower().replace('_', ' ') + ' '
+    return re.match(r'^.* (men|man|males?|girls?|boys?) .*$', wacky_string) != None
 
-# Sexist datasets
+def has_female(string):
+    wacky_string = ' ' + string.lower().replace('_', ' ') + ' '
+    return re.match(r'^.* (women|woman|females?|girls?) .*$', wacky_string) != None
+
+# Subset the datasets
 def gendered(pair):
     uri, column_names = pair
-    columns_blob = ' ' + ' '.join(column_names).lower().replace('_', ' ') + ' '
-    return ' men ' in columns_blob or ' man ' in columns_blob or ' male ' in columns_blob
+    columns_blob = ' '.join(column_names)
+    return has_gender(columns_blob)
 
-columns = list(map_reduce(lambda d: (d['uri'], d['columns']), datasets = datasets))
-gendered_datasets = filter(gendered, columns)
+# How many "male" and "female"?
+def how_many(pair):
+    '''
+    Args:
+        A tuple of (uri, column_names)
+    Returns:
+        A dictionary
+    '''
+    uri, column_names = pair
+    n_gendered = len(filter(has_gender, column_names))
+    n_female   = len(filter(has_female, column_names))
+    return {
+        'uri': uri,
+        'n_gendered': n_gendered,
+        'n_female': n_female,
+    }
 
-print '%d datasets appear to have gender in the column names:' % len(gendered_datasets)
-print [d[0] for d in gendered_datasets]
-print ''
+def go():
+    # load the data into memory
+    datasets = list(iter_datasets())
+
+    # Subset
+    columns = list(map_reduce(lambda d: (d['uri'], d['columns']), datasets = datasets))
+    gendered_datasets = filter(gendered, columns)
+
+    df = pandas.DataFrame(map(how_many, gendered_datasets))[['uri', 'n_gendered', 'n_female']]
+
+    # Results
+    print '%d datasets appear to have gender in the column names:' % len(gendered_datasets)
+    print [d[0] for d in gendered_datasets]
+    print ''
