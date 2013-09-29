84113d2eddec4c42b9b1c21dd791b5804f01dee0
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 11:42:12 -0700

    plot-licensing.r

---
+++ plot-licensing.r
@@ -0,0 +1,10 @@
+library(ggplot2)
+
+l <- read.csv('licensing.csv')
+l$prop <- l$no_license / l$all
+
+
+p1 <- ggplot(l) + aes(x = prop) + geom_histogram() +
+  scale_x_continuous('Proportion of datasets with a the license field filled in') +
+  scale_y_continuous('Number of portals with this proportion') +
+  ggtitle('Which portals indicate the licenses of their datasets?')
