60b00a7fcb4273cab0ec99d434a0c134d83e4234
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 12:39:24 -0700

    plots

--- plot-licensing.r
+++ plot-licensing.r
@@ -11,10 +11,10 @@ p1 <- ggplot(lb) + aes(x = prop) + geom_histogram() +
   scale_y_continuous('Number of portals with this proportion') +
   ggtitle('Which portals indicate the licenses of their datasets?')
 
-p2 <- ggplot(l) + aes(x = license) + geom_bar()
+p2 <- ggplot(l) + aes(x = license_standard) + geom_bar()
 
-p3 <- ggplot(subset(l, portal == 'data.sfgov.org' | portal == 'data.cityofnewyork.us' | portal == 'data.cityofchicago.us' | portal == 'data.gov.uk' | portal == 'explore.data.gov')) +
-  facet_wrap(~ portal) + aes(x = license, fill = portal_software) + geom_bar() +
+p3 <- ggplot(subset(l, portal == 'data.sfgov.org' | portal == 'data.cityofnewyork.us' | portal == 'data.cityofchicago.us' | portal == 'data.gov.uk' | portal == 'explore.data.gov' | portal == 'data.cityofsantacruz.com' | portal == 'parisdata.opendatasoft.com' | portal == 'data.oaklandnet.com')) +
+  facet_wrap(~ portal) + aes(x = license_standard, fill = portal_software) + geom_bar() +
   coord_flip()
 
 p <- function(.p, fn) {
@@ -23,6 +23,6 @@ p <- function(.p, fn) {
   dev.off()
 }
 
-# p(p1, 'p1.png')
-# p(p2, 'p2.png')
+p(p1, 'p1.png')
+p(p2, 'p2.png')
 p(p3, 'p3.png')
