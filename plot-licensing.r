6596aa6d129613425b4cf0d3a1be2952efd36c06
Thomas Levine <occurrence@thomaslevine.com>
2013-10-02 14:57:43 +0000

    more plots

--- plot-licensing.r
+++ plot-licensing.r
@@ -19,18 +19,31 @@ p1 <- ggplot(lb) + aes(x = prop) + geom_histogram() +
   scale_y_continuous('Number of portals with this proportion') +
   ggtitle('Which portals indicate the licenses of their datasets?')
 
-p2 <- ggplot(l) + aes(x = license_standard) + geom_bar() + coord_flip() +
+p2 <- ggplot(l) + aes(x = license.reduced) + geom_bar() + coord_flip() +
   scale_x_discrete('Dataset license') +
   scale_y_continuous('Number of datasets') +
   ggtitle('Data license in use across 100 portals')
 
 p3 <- ggplot(subset(l, portal == 'data.sfgov.org' | portal == 'data.cityofnewyork.us' | portal == 'data.cityofchicago.us' | portal == 'data.gov.uk' | portal == 'explore.data.gov' | portal == 'data.cityofsantacruz.com' | portal == 'parisdata.opendatasoft.com' | portal == 'data.oaklandnet.com')) +
-  facet_wrap(~ portal) + aes(x = license_standard) +  geom_bar() +
+  facet_wrap(~ portal) + aes(x = license.reduced) +  geom_bar() +
   # aes(fill = portal_software) +
   coord_flip() +
   scale_x_discrete('Dataset license') +
   scale_y_continuous('Number of datasets') +
   ggtitle('Licenses in use in a few portals')
+
+plot.wrapped <- function(data) {
+  ggplot(data) +
+    facet_wrap(~ portal) + aes(x = license.reduced) +  geom_bar() +
+    aes(fill = portal_software) +
+    coord_flip() +
+    scale_x_discrete('Dataset license') +
+    scale_y_continuous('Number of datasets') +
+    ggtitle('Dataset license by portal')
+}
+
+p4 <- plot.wrapped(l)
+p5 <- plot.wrapped(subset(l, portal != 'publicdata.eu'))
 
 p <- function(.p, fn) {
   png(fn, width = 840, height = 840, res = 150)
@@ -38,6 +51,16 @@ p <- function(.p, fn) {
   dev.off()
 }
 
-p(p1, 'p1.png')
-p(p2, 'p2.png')
-p(p3, 'p3.png')
+main <- function() {
+  p(p1, 'p1.png')
+  p(p2, 'p2.png')
+  p(p3, 'p3.png')
+}
+
+png('p4.png',  width = 2 * 840, height = 2 * 840, res = 100)
+print(p4)
+dev.off()
+
+png('p5.png',  width = 2 * 840, height = 2 * 840, res = 100)
+print(p5)
+dev.off()
